ee1e9934f900622eea6e985478759fd583d71b10
B Howard <ben.howard@asos.com>
2021-05-21 17:56:00 +0100

    2nd commit

---
+++ validateVariablesTask/validateVariables.ps1
@@ -0,0 +1,35 @@
+[CmdletBinding()]
+param()
+# https://raw.githubusercontent.com/Microsoft/vsts-tasks/master/Tasks/MSBuildV1/MSBuild.ps1
+
+Trace-VstsEnteringInvocation $MyInvocation
+try {
+    Import-VstsLocStrings "$PSScriptRoot\Task.json"
+
+    # Get the inputs.
+    [string]$warnOrError = Get-VstsInput -Name $warnOrError
+
+    $UnresolvedVariables = @()
+
+    # Get unresolved variables
+    Get-ChildItem env:* | ForEach-Object {
+        if ($_.Value -match $regex) {
+            $UnresolvedVariables += $_
+            Write-Host ("Variable {0} is unresolved. Value is {1}" -f $_.Name, $_.Value) -ForegroundColor Yellow
+        }        
+    }
+
+    if ($UnresolvedVariables.Count -gt 0) {
+        $Msg = ("{0} unresolved variables were found" -f $UnresolvedVariables.Count  )
+
+        if($warnOrError -eq 'warn') {
+            Write-Warning $Msg
+        }
+        else {
+            Throw $Msg
+        }
+    }   
+    
+} finally {
+    Trace-VstsLeavingInvocation $MyInvocation
+}
