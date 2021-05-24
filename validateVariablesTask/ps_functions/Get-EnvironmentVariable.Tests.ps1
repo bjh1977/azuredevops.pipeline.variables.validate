76487055504a7733a4999f1a23843603bdb0fa51
bjh1977 <bjh1977@users.noreply.github.com>
2021-05-24 12:53:31 +0100

    Feature/add tests (#1)
    * Refactor, add tests
    
    * test result path
    
    * naming
    
    * naming again
    
    * path
    
    * debug
    
    * Add test
    
    * output result
    
    * Scope properly
    
    Co-authored-by: B Howard <ben.howard@asos.com>

---
+++ validateVariablesTask/ps_functions/Get-EnvironmentVariable.Tests.ps1
@@ -0,0 +1,74 @@
+BeforeAll {
+    . $PSCommandPath.Replace('.Tests.ps1','.ps1')
+}
+
+Describe Get-EnvironmentVariable {
+
+    
+
+    BeforeAll {
+        $UnresolvedRegex = '\$\(.*?\)'
+        $env:GetEnvVarTestVar1 = 'GetEnvVarTestVar1Value'
+        $env:GetEnvVarTestVar1a = 'GetEnvVarTestVar1aValue'
+        $env:GetEnvVarTestVar2 = 'GetEnvVarTestVar2Value'
+        $env:GetEnvVarTestVar2a = 'GetEnvVarTestVar2aValue$(Unresolved)'
+        $env:GetEnvVarTestVar3 = 'GetEnvVarTestVar3Value$(Unresolved)-Something($Unresolved)'
+    }
+
+    BeforeEach {
+        $TestResult = $null
+    }
+    
+
+    It "Returns no results when no variables match" {
+        $TestResult = Get-EnvironmentVariable -NameRegex '^DoesNotExist$'
+        $TestResult | Should -Be $null
+    }
+
+    It "Returns 1 result when 1 variable matches" {
+        $TestResult = Get-EnvironmentVariable -NameRegex '^GetEnvVarTestVar1$'
+        $TestResult.Count | Should -Be 1
+    }
+
+    It "Returns 2 results when 2 variables match" {
+        $TestResult = Get-EnvironmentVariable -NameRegex '^GetEnvVarTestVar1'
+        $TestResult.Count | Should -Be 2
+    }
+
+    It "Returns 0 results when 1 variable matches and is resolved" {
+        $TestResult = Get-EnvironmentVariable -NameRegex '^GetEnvVarTestVar2$' -ValueRegex $UnresolvedRegex
+        $TestResult | Should -Be $null
+    }
+
+    It "Returns 1 result when 1 variable matched and is unresolved" {
+        $TestResult = Get-EnvironmentVariable -NameRegex '^GetEnvVarTestVar2a$' -ValueRegex $UnresolvedRegex
+        $TestResult.Count | Should -Be 1
+    }
+
+    It "Returns 1 result when 2 variables match and 1 is unresolved" {
+        $TestResult = Get-EnvironmentVariable -NameRegex '^GetEnvVarTestVar2' -ValueRegex $UnresolvedRegex
+        $TestResult.Count | Should -Be 1
+    }
+
+    It "Returns 1 result when 1 variable matched with multiple unresolved values" {
+        $TestResult = Get-EnvironmentVariable -NameRegex '^GetEnvVarTestVar3$' -ValueRegex $UnresolvedRegex
+        $TestResult.Count | Should -Be 1
+    }
+
+    AfterAll {
+        $env:GetEnvVarTestVar1 = $null
+        $env:GetEnvVarTestVar1a = $null
+        $env:GetEnvVarTestVar2 = $null
+        $env:GetEnvVarTestVar2a = $null
+        $env:GetEnvVarTestVar3 = $null
+    }
+   
+
+    
+
+    
+
+
+}
+
+
---
+++ validateVariablesTask/ps_functions/Get-EnvironmentVariable.ps1
@@ -0,0 +1,14 @@
+function Get-EnvironmentVariable {
+    [cmdletbinding()]
+    param (
+        [string]$NameRegex = '.*',
+        [string]$ValueRegex = '.*'
+        )
+
+    Write-Verbose ("Getting environment variable with Name regex of '{0}' and Value regex of '{1}'" -f $NameRegex, $ValueRegex )
+    
+    $result = Get-ChildItem env:* | Where-Object {$_.Name -match $NameRegex -and $_.Value -match $ValueRegex}
+
+    return $result
+
+}
